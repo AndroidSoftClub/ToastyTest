8e10d4804aa252fceeb3ef77489e26cf3ca49def
Fenil Lathiya <lathiyafenil518@gmail.com>
2023-12-27 23:06:41 +0530

    ui done

--- FenilToasty/src/main/java/com/example/feniltoasty/FenilToasty.kt
+++ FenilToasty/src/main/java/com/example/feniltoasty/FenilToasty.kt
@@ -3,7 +3,7 @@ package com.example.feniltoasty
 import android.app.Activity
 import android.widget.Toast
 
-object FenilToasty {
+class FenilToasty {
 
     fun Toastfenil(activity: Activity, txt: String) =
         Toast.makeText(activity, txt, Toast.LENGTH_LONG).show()
